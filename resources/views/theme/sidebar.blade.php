21b677d5c93240cc6459df3b1e7906b12bae8114
MARKOM-PC <dev@bprbangunarta.co.id>
2023-11-15 18:04:20 +0700

    Fix

--- resources/views/theme/sidebar.blade.php
+++ resources/views/theme/sidebar.blade.php
@@ -8,13 +8,12 @@
                 <!-- profile box -->
                 <div class="profileBox">
                     <div class="image-wrapper">
-                        <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="imaged rounded">
+                        <img src="{{ asset('assets/img/favicon.png') }}" alt="image" class="imaged rounded">
                     </div>
                     <div class="in">
-                        <strong>Julian Gruber</strong>
+                        <strong class="text-uppercase">{{ Auth::user()->username }}</strong>
                         <div class="text-muted">
-                            <ion-icon name="location"></ion-icon>
-                            California
+                            {{ Auth::user()->kantor_kode }} | {{ Auth::user()->code_user }} | {{ Auth::user()->kode_surveyor }}
                         </div>
                     </div>
                     <a href="javascript:;" class="close-sidebar-button" data-dismiss="modal">
@@ -25,126 +24,52 @@
 
                 <ul class="listview flush transparent no-line image-listview mt-2">
                     <li>
-                        <a href="index.html" class="item">
+                        <a href="/dashboard" class="item">
                             <div class="icon-box bg-primary">
                                 <ion-icon name="home-outline"></ion-icon>
                             </div>
                             <div class="in">
-                                Discover
+                                Home
                             </div>
                         </a>
                     </li>
-                    <li>
-                        <a href="app-components.html" class="item">
-                            <div class="icon-box bg-primary">
-                                <ion-icon name="cube-outline"></ion-icon>
-                            </div>
-                            <div class="in">
-                                Components
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="app-pages.html" class="item">
-                            <div class="icon-box bg-primary">
-                                <ion-icon name="layers-outline"></ion-icon>
-                            </div>
-                            <div class="in">
-                                <div>Pages</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="page-chat.html" class="item">
-                            <div class="icon-box bg-primary">
-                                <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
-                            </div>
-                            <div class="in">
-                                <div>Chat</div>
-                                <span class="badge badge-danger">5</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <div class="item">
-                            <div class="icon-box bg-primary">
-                                <ion-icon name="moon-outline"></ion-icon>
-                            </div>
-                            <div class="in">
-                                <div>Dark Mode</div>
-                                <div class="custom-control custom-switch">
-                                    <input type="checkbox" class="custom-control-input dark-mode-switch"
-                                        id="darkmodesidebar">
-                                    <label class="custom-control-label" for="darkmodesidebar"></label>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
 
-                <div class="listview-title mt-2 mb-1">
-                    <span>Friends</span>
-                </div>
-                <ul class="listview image-listview flush transparent no-line">
                     <li>
-                        <a href="page-chat.html" class="item">
-                            <img src="assets/img/sample/avatar/avatar7.jpg" alt="image" class="image">
+                        <a href="/survey" class="item">
+                            <div class="icon-box bg-primary">
+                                <ion-icon name="pie-chart-outline"></ion-icon>
+                            </div>
                             <div class="in">
-                                <div>Sophie Asveld</div>
+                                Survey
                             </div>
                         </a>
                     </li>
+                    
                     <li>
-                        <a href="page-chat.html" class="item">
-                            <img src="assets/img/sample/avatar/avatar3.jpg" alt="image" class="image">
-                            <div class="in">
-                                <div>Sebastian Bennett</div>
-                                <span class="badge badge-danger">6</span>
+                        <a href="/tracking" class="item">
+                            <div class="icon-box bg-primary">
+                                <ion-icon name="file-tray-full-outline"></ion-icon>
                             </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="page-chat.html" class="item">
-                            <img src="assets/img/sample/avatar/avatar10.jpg" alt="image" class="image">
                             <div class="in">
-                                <div>Beth Murphy</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="page-chat.html" class="item">
-                            <img src="assets/img/sample/avatar/avatar2.jpg" alt="image" class="image">
-                            <div class="in">
-                                <div>Amelia Cabal</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="page-chat.html" class="item">
-                            <img src="assets/img/sample/avatar/avatar5.jpg" alt="image" class="image">
-                            <div class="in">
-                                <div>Henry Doe</div>
+                                Tracking
                             </div>
                         </a>
                     </li>
                 </ul>
-
             </div>
 
             <!-- sidebar buttons -->
             <div class="sidebar-buttons">
-                <a href="javascript:;" class="button">
+                <a href="/setting" class="button bg-primary">
                     <ion-icon name="person-outline"></ion-icon>
                 </a>
-                <a href="javascript:;" class="button">
-                    <ion-icon name="archive-outline"></ion-icon>
-                </a>
-                <a href="javascript:;" class="button">
-                    <ion-icon name="settings-outline"></ion-icon>
-                </a>
-                <a href="javascript:;" class="button">
-                    <ion-icon name="log-out-outline"></ion-icon>
-                </a>
+
+                <form method="POST" action="{{ route('logout') }}" style="width: 100%;">
+                @csrf
+                    <a href="#" class="button bg-danger" onclick="event.preventDefault(); this.closest('form').submit();">
+                        <ion-icon name="log-out-outline"></ion-icon>
+                    </a>
+                </form>
             </div>
             <!-- * sidebar buttons -->
         </div>
